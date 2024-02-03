5a863cfba6e09c0103eacf9e87fae9851f47cd82
Mihai <mcosma@gmail.com>
2024-02-02 22:08:25 -0500

    happiness

--- predict.py
+++ predict.py
@@ -2,11 +2,14 @@
 from utils.check_env import running_interactive
 from collections import namedtuple
 import os
+import time
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import wandb
 from matplotlib import pyplot as plt
+
+RUNNING_INTERACTIVE = running_interactive()
 
 config = wandb.config if "WANDB_RUN_ID" in os.environ else {}
 
@@ -15,7 +18,6 @@ config = wandb.config if "WANDB_RUN_ID" in os.environ else {}
 Normal = namedtuple("Normal", ["mean", "std"])
 num_blocks = 1000 if "num_blocks" not in config else int(config["num_blocks"])
 rate = Normal(0.035, 0.02)
-trade = Normal(0.002, 0.001)
 randseed = 0 if "randseed" not in config else int(config["randseed"])
 
 # %%
@@ -24,50 +26,98 @@ randseed = 0 if "randseed" not in config else int(config["randseed"])
 rate_histories = []
 records = []
 min_fees = 0.0001
-max_fees = 0.01
-increment = 0.0001
-for fees in np.arange(min_fees, max_fees + increment, increment):
-    for epoch in range(100):
+max_fees = 1
+increment = 0.01
+trades = 0
+start_time = time.time()
+debug = False
+for fee_rate in np.arange(min_fees, max_fees + increment, increment):
+# for fee_rate in [max_fees]:
+    for epoch in range(10):
+    # for epoch in range(1):
         rng = np.random.default_rng(seed=epoch)
         rate_history = [rate.mean]
         volume = 0
+        trader_profit = 0
+        lp_profit = 0
         for _ in range(1, num_blocks):
             # every trader has a different target rate
             target = rng.normal(rate.mean, rate.std)
             # trade if rate is outside of dead zone around their target
-            diff_to_true_mean = abs(rate_history[-1] - target)
-            if diff_to_true_mean > fees:
-                tradesize = min(max(0, rng.normal(trade.mean, trade.std)), diff_to_true_mean)
+            diff_to_target = abs(rate_history[-1] - target)
+            fees = rate_history[-1] * fee_rate
+            if debug:
+                print(f"{target=} {diff_to_target=} {fees=}")
+            if diff_to_target > fees:
                 direction = -1 if target < rate_history[-1] else 1
-                new_rate = rate_history[-1] + direction * tradesize
+                new_rate = rate_history[-1] + direction * diff_to_target
                 rate_history.append(new_rate)
-                volume += abs(rate_history[-1] - rate_history[-2])
-        money_made = volume * fees
-        # print(f"fees={fees}, money_made={money_made}, volume={volume}")
-        records.append((fees, epoch, money_made, volume))
+                rate_change = abs(rate_history[-1] - rate_history[-2])
+                volume += rate_change
+                trader_profit += rate_change - fees
+                lp_profit += fees
+                if debug:
+                    print(f"{rate_change=} {fees=} {trader_profit=}")
+                trades += 1
+        records.append((fee_rate, epoch, trader_profit, lp_profit, volume))
         rate_histories.append(rate_history)
+        if debug:
+            plt.plot(rate_history)
+print(f"simulated {trades=} trades in {time.time() - start_time} seconds")
 
 # %%
-# analysis
-if running_interactive():
+# create dataframe
+if RUNNING_INTERACTIVE:
     rh_df = pd.DataFrame(rate_histories)
-    plt.plot(rh_df.values.T, alpha=0.01, color="black")
-    plt.show()
-    results = pd.DataFrame(records, columns=["fees", "epoch", "money_made", "volume"])
+    results = pd.DataFrame(records, columns=["fee_rate", "epoch", "trader_profit", "lp_profit", "volume"])
+    results["happiness"] = results["lp_profit"] + results["trader_profit"] * 0.5
     display(results)
 
-    sns.relplot(x="fees", y="money_made", kind="line", data=results, color="blue")
-    plt.ylabel("money_made", color="blue")
-    plt.tick_params(axis='y', color="blue", labelcolor="blue")
-    plt.gca().spines['left'].set_color("blue")
-    plt.title("money_made vs. fees")
-    plt.show()
+# %%
+# rate history
+if RUNNING_INTERACTIVE:
+    plt.plot(rh_df.values.T, alpha=0.005, color="black")
+    plt.show()  
 
-    sns.relplot(x="fees", y="volume", kind="line", data=results, color="red")
+# %%
+# subplots
+fig, ax = plt.subplots(3, 1, figsize=(10, 10))
+
+# lp profit
+if RUNNING_INTERACTIVE:
+    sns.lineplot(x="fee_rate", y="lp_profit", data=results, color="blue", ax=ax[0], label="lp profit")
+    plt.ylabel("profit", color="blue")
+    ax[0].tick_params(axis='y', color="blue", labelcolor="blue")
+    ax[0].spines['left'].set_color("blue")
+    ax[0].legend()
+
+# trader profit
+if RUNNING_INTERACTIVE:
+    sns.lineplot(x="fee_rate", y="trader_profit", data=results, color="orange", ax=ax[1], label="trader profit")
+    plt.ylabel("profit", color="orange")
+    ax[1].tick_params(axis='y', color="orange", labelcolor="orange")
+    ax[1].spines['left'].set_color("orange")
+    ax[1].legend()
+
+# volume
+if RUNNING_INTERACTIVE:
+    sns.lineplot(x="fee_rate", y="volume", data=results, color="red", ax=ax[2], label="volume")
     plt.ylabel("volume", color="red")
-    plt.tick_params(axis='y', color='red', labelcolor='red')
-    plt.gca().spines['left'].set_color("red")
-    plt.title("volume vs. fees")
-    plt.show()
+    ax[2].tick_params(axis='y', color='red', labelcolor='red')
+    ax[2].spines['left'].set_color("red")
+    ax[2].legend()
+
+# %%
+fig, ax = plt.subplots(1, 1, figsize=(6, 4))
+sns.lineplot(x="fee_rate", y="lp_profit", data=results, color="blue", ax=ax, label="lp profit")
+sns.lineplot(x="fee_rate", y="trader_profit", data=results, color="orange", ax=ax, label="trader profit")
+sns.lineplot(x="fee_rate", y="volume", data=results, color="red", ax=ax, label="volume")
+sns.lineplot(x="fee_rate", y="happiness", data=results, color="green", ax=ax, label="happiness")
+plt.xlabel("fees")
+plt.ylabel("profit (% points)")
+# find index where happiness is max
+max_index = results["happiness"].idxmax()
+plt.title(f"max happiness at fees={results['fee_rate'][max_index]}")
+plt.legend();
 
 # %%
