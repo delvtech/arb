97d80832f9b6554aba6079d327c46e42a07bb82f
Mihai <mcosma@gmail.com>
2024-02-07 14:18:23 -0500

    simplify

--- predict.py
+++ predict.py
@@ -8,47 +8,22 @@ import pandas as pd
 import seaborn as sns
 import wandb
 from matplotlib import pyplot as plt
-from scipy.optimize import fsolve
 
 RUNNING_INTERACTIVE = running_interactive()
 
 config = wandb.config if "WANDB_RUN_ID" in os.environ else {}
 
 # %%
-# plot budget curve
-mode = 0.002
-
-# Assuming some values for k (shape) and theta (scale) - these would need to be adjusted
-k = 2.5  # Example shape parameter, needs fine-tuning
-theta = mode / (k - 1)  # Approximate scale parameter based on mode formula
-
-# Generate samples
-samples = np.random.gamma(shape=k, scale=theta, size=10000)
-
-# Plot histogram to visualize
-plt.hist(samples, bins=100, density=True, label=f"mode={mode} mean={np.mean(samples)}")
-plt.xlabel("budget in terms of rate change")
-plt.legend()
-plt.show()
-
-# Calculate and print actual mode and standard deviation of the samples
-print(f"Sample Mode Approximation: {mode}")  # Mode approximation based on input
-actual_std = np.std(samples)
-print(f"Actual Standard Deviation of Samples: {actual_std}")
-
-# %%
 # setup
 Normal = namedtuple("Normal", ["mean", "std"])
-Gamma = namedtuple("Gamma", ["k", "theta"])
 num_blocks = 1000 if "num_blocks" not in config else int(config["num_blocks"])
-rate = Normal(0.035, 0.02)
-budget = Gamma(k, theta)
+rate = Normal(0.035, 0.005)
 randseed = 0 if "randseed" not in config else int(config["randseed"])
 
 # %%
-# run shit
-# fees = 0.005 if "fees" not in config else float(config["fees"])
+# run simulation
 rate_histories = []
+target_histories = []
 records = []
 min_fees = 0.0001
 max_fees = 1
@@ -57,37 +32,41 @@ trades = 0
 start_time = time.time()
 debug = False
 for fee_rate in np.arange(min_fees, max_fees + increment, increment):
-# for fee_rate in [max_fees]:
+# for fee_rate in [0.2]:
     for epoch in range(100):
     # for epoch in range(1):
         rng = np.random.default_rng(seed=epoch)
         rate_history = [rate.mean]
+        target_history = [np.nan]
         volume = 0
-        trader_profit = 0
+        trader_happiness = 0
         lp_profit = 0
-        for _ in range(1, num_blocks):
+        for block in range(1, num_blocks):
             # every trader has a different target rate
             target = rng.normal(rate.mean, rate.std)
-            tradebudget = rng.gamma(budget.k, budget.theta)
+            target_history.append(target)
             # trade if rate is outside of dead zone around their target
             diff_to_target = abs(rate_history[-1] - target)
             fees = rate_history[-1] * fee_rate
             if debug:
                 print(f"{target=} {diff_to_target=} {fees=}")
-            if diff_to_target > fees and tradebudget > fees:
+            if diff_to_target > fees:
                 direction = -1 if target < rate_history[-1] else 1
-                tradesize = max(0, min(diff_to_target, tradebudget))
+                tradesize = max(0, diff_to_target-fees)
                 new_rate = rate_history[-1] + direction * tradesize
                 rate_history.append(new_rate)
                 rate_change = abs(rate_history[-1] - rate_history[-2])
                 volume += rate_change
-                trader_profit += rate_change - fees
+                trader_happiness += rate_change
                 lp_profit += fees
                 if debug:
-                    print(f"{rate_change=} {fees=} {trader_profit=}")
+                    print(f"{block=} {rate_change=} {fees=} {trader_happiness=}")
                 trades += 1
-        records.append((fee_rate, epoch, trader_profit, lp_profit, volume))
+            else:
+                rate_history.append(rate_history[-1])
+        records.append((fee_rate, epoch, trader_happiness, lp_profit, volume))
         rate_histories.append(rate_history)
+        target_histories.append(target_history)
         if debug:
             plt.plot(rate_history)
 print(f"simulated {trades=} trades in {time.time() - start_time} seconds")
@@ -96,25 +75,44 @@ print(f"simulated {trades=} trades in {time.time() - start_time} seconds")
 # create dataframe
 if RUNNING_INTERACTIVE:
     rh_df = pd.DataFrame(rate_histories)
-    results = pd.DataFrame(records, columns=["fee_rate", "epoch", "trader_profit", "lp_profit", "volume"])
-    results["happiness"] = results["lp_profit"] + results["trader_profit"] * 0.5
-    display(results)
+    target_df = pd.DataFrame(target_histories)
+    results = pd.DataFrame(records, columns=["fee_rate", "epoch", "trader_happiness", "lp_profit", "volume"])
+    results["happiness"] = results["lp_profit"] + results["trader_happiness"] * 0.5
+    # display(results)
 
 # %%
-# rate history
+# inspect one trade history
 if RUNNING_INTERACTIVE:
+    plt.figure(figsize=(8, 4))
+    plt.plot(rh_df.values.T[:,0], alpha=1, color="blue", label="market rate", linewidth=0.5)
+    errors = [t*fee_rate for t in target_histories[0]]
+    plt.scatter(np.arange(len(target_histories[0])), target_histories[0], color="red", s=1, label="target rate")
+    # plt.errorbar(np.arange(len(target_histories[0])), target_histories[0], color="red", label="target rate", yerr=errors, elinewidth=0.5, capsize=0.5)
+    plt.xlabel("block number")
+    plt.ylabel("fixed rate (%)")
+    plt.xlim(0, 100)
+    plt.legend()
+    plt.show()
+
+# %%
+# plot all rate histories
+if RUNNING_INTERACTIVE:
+    plt.figure(figsize=(8, 4))
     plt.plot(rh_df.values.T, alpha=0.005, color="black")
+    plt.xlabel("block number")
+    plt.ylabel("fixed rate (%)")
+    plt.title("all rate paths")
     plt.show()  
 
 # %%
 # plot together
 if RUNNING_INTERACTIVE:
-    fig, ax = plt.subplots(1, 1, figsize=(6, 4))
+    fig, ax = plt.subplots(1, 1, figsize=(8, 4))
     sns.lineplot(x="fee_rate", y="lp_profit", data=results, color="blue", ax=ax, label="lp profit", errorbar="pi")
-    sns.lineplot(x="fee_rate", y="trader_profit", data=results, color="orange", ax=ax, label="trader profit", errorbar="pi")
+    # sns.lineplot(x="fee_rate", y="trader_happiness", data=results, color="orange", ax=ax, label="trader happiness", errorbar="pi")
     sns.lineplot(x="fee_rate", y="volume", data=results, color="red", ax=ax, label="volume", errorbar="pi")
-    sns.lineplot(x="fee_rate", y="happiness", data=results, color="green", ax=ax, label="happiness", errorbar="pi")
-    plt.xlim([0, 0.2])
+    sns.lineplot(x="fee_rate", y="happiness", data=results, color="green", ax=ax, label="total happiness", errorbar="pi")
+    plt.xlim([0, 1])
     plt.xlabel("fees")
     plt.ylabel("profit (% points)")
     # find index where happiness is max
